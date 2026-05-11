5fe260d4531fb5773dc42a591e4473ec33f61338
rahwan10 <donggeon10@naver.com>
2026-05-11 23:39:48 +0900

    fix: key값 반영

--- src/components/Main/CategoryFilter.jsx
+++ src/components/Main/CategoryFilter.jsx
@@ -16,7 +16,9 @@ function CategoryFilter({ category, setCategory }) {
       >
         <option value="전체">전체</option>
         {foodCategory.map((r) => (
-          <option value={r}>{r}</option>
+          <option key={r} value={r}>
+            {r}
+          </option>
         ))}
       </select>
     </section>
